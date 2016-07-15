f0b7125cead278babc9f6989b470faf01b8ef167
Julio Vilar <juliovilar93@gmail.com>
2016-07-15 18:03:59 -0300

    arquitetura de pastas

---
+++ model/classes/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: julio
+ * Date: 15/07/16
+ * Time: 18:00
+ */
+class Usuario{
+
+    var $id;
+    var $login;
+    var $senha;
+    var $email;
+
+}
